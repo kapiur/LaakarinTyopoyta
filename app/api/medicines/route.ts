2004191b7f956ad8b81a9afe34f3b94f0edcfcc5
kapiur <122556344+kapiur@users.noreply.github.com>
2026-01-19 19:57:43 +0200

    Update route.ts

--- app/api/medicines/route.ts
+++ app/api/medicines/route.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// Это предотвращает создание множества подключений к БД при каждом запросе
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
