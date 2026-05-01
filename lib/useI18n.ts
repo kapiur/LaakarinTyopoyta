d5e3c21eb5e700db3bb0d8426de4d62ede836825
kapiur <122556344+kapiur@users.noreply.github.com>
2026-05-01 20:49:53 +0300

    Use I18nProvider from useI18n hook

--- lib/useI18n.ts
+++ lib/useI18n.ts
@@ -1,15 +1,3 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { translate, type TranslationKey } from "./i18n";
-import { normalizeUiLanguage } from "./i18n/config";
-
-export function useI18n() {
-  const { data: session } = useSession();
-  const language = normalizeUiLanguage((session?.user as any)?.uiLanguage);
-
-  return {
-    language,
-    t: (key: TranslationKey) => translate(language, key),
-  };
-}
+export { useI18nContext as useI18n } from "../components/I18nProvider";
