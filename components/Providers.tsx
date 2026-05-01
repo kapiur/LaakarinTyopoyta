921f53817aca4329498f45a384c851ac406d438e
kapiur <122556344+kapiur@users.noreply.github.com>
2026-05-01 21:10:53 +0300

    Enable templates i18n enhancer

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,12 +2,14 @@
 import { SessionProvider } from "next-auth/react";
 import PasswordChangeGuard from "./PasswordChangeGuard";
 import { I18nProvider } from "./I18nProvider";
+import TemplatesI18nEnhancer from "./TemplatesI18nEnhancer";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <I18nProvider>
         <PasswordChangeGuard />
+        <TemplatesI18nEnhancer />
         {children}
       </I18nProvider>
     </SessionProvider>
