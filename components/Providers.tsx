970aa6f65570c39832dd703558c0c84bf5127cef
kapiur <122556344+kapiur@users.noreply.github.com>
2026-05-01 21:42:22 +0300

    Enable enhanced Mallit syntax help

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,12 +2,14 @@
 import { SessionProvider } from "next-auth/react";
 import PasswordChangeGuard from "./PasswordChangeGuard";
 import { I18nProvider } from "./I18nProvider";
+import MalliSyntaxHelpEnhancer from "./MalliSyntaxHelpEnhancer";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <I18nProvider>
         <PasswordChangeGuard />
+        <MalliSyntaxHelpEnhancer />
         {children}
       </I18nProvider>
     </SessionProvider>
