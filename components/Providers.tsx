84e694c7d1a84561b93a55844f79b6d04244fc65
kapiur <122556344+kapiur@users.noreply.github.com>
2026-05-01 19:54:23 +0300

    Run password change guard inside session provider

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+import PasswordChangeGuard from "./PasswordChangeGuard";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <PasswordChangeGuard />
+      {children}
+    </SessionProvider>
+  );
 }
