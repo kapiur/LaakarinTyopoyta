3f8a9abb3700fe0f557aba214ef282ec603429d3
kapiur <122556344+kapiur@users.noreply.github.com>
2026-05-01 20:50:09 +0300

    Wrap app in I18nProvider

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import PasswordChangeGuard from "./PasswordChangeGuard";
+import { I18nProvider } from "./I18nProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <PasswordChangeGuard />
-      {children}
+      <I18nProvider>
+        <PasswordChangeGuard />
+        {children}
+      </I18nProvider>
     </SessionProvider>
   );
 }
